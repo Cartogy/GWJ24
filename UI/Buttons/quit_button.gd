b0cb9e3f9661aef5e931978f7f535cac4718987a
Jonas <jonas_munz@outlook.com>
2020-08-22 17:08:44 -0500

    Finished integrating UI to levels

--- UI/Buttons/quit_button.gd
+++ UI/Buttons/quit_button.gd
@@ -8,9 +8,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.connect("pressed", self, "quit")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func quit():
+	get_tree().quit()
