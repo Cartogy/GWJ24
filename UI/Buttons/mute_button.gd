19838e5fd652e67afd7cff0c2181bac2dda8d760
Jonas <jonas_munz@outlook.com>
2020-08-22 03:04:44 -0500

    Added new textures + mute button

---
+++ UI/Buttons/mute_button.gd
@@ -0,0 +1,13 @@
+extends TextureButton
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.connect("pressed", get_parent(), "toggle")
+	# connect to another function here
+
---
+++ UI/Buttons/mute_manager.gd
@@ -0,0 +1,31 @@
+extends Node
+
+
+#onready var packed_mute_off_button = load("res://UI/Buttons/MuteOffButton.tscn")
+# Have Mute_off button be active in editor view
+onready var packed_mute_on_button = load("res://UI/Buttons/MuteOnButton.tscn")
+
+var mute_on_button
+var mute_off_button
+
+# Allows for simple toggle behaviour 
+enum MuteState { OFF, ON }
+var current_state
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	mute_on_button = packed_mute_on_button.instance()
+	#mute_off_button = packed_mute_off_button.instance()
+	mute_off_button = get_child(0)
+	current_state = MuteState.OFF
+
+func toggle():
+	match current_state:
+		MuteState.OFF:
+			remove_child(mute_off_button)
+			add_child(mute_on_button)
+			current_state = MuteState.ON
+		MuteState.ON:
+			remove_child(mute_on_button)
+			add_child(mute_off_button)
+			current_state = MuteState.OFF
